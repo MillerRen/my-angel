572b019fde472dff808c3098548524f68564964b
Miller Ren <ren12345@126.com>
2015-06-24 18:21:30 +0800

    add browser-sync config

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,18 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+// watch files for changes and reload
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'client'
+    }
+  });
+
+  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'app'}, reload);
+});
+
 
 gulp.task('default', function() {
   
