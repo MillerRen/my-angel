4ae35abec4779cc9a84e0e7def2047ca2e22239b
Miller Ren <ren12345@126.com>
2015-08-04 14:24:18 +0800

    revert

--- server/config/express.js
+++ server/config/express.js
@@ -23,8 +23,8 @@ module.exports = function(app) {
   var env = app.get('env');
 
   app.set('views', config.root + '/server/views');
-  //app.engine('html', require('jade'));
-  app.set('view engine', 'jade');
+  app.engine('html', require('ejs').renderFile);
+  app.set('view engine', 'html');
   app.use(compression());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
